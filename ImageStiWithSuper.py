417cc1c0e667556e01ae791526bec58da6cb30fc
pkshi26 <48425090+pkshi26@users.noreply.github.com>
2022-04-09 15:59:44 +0800

    Update ImageStiWithSuper.py

--- ImageStiWithSuper.py
+++ ImageStiWithSuper.py
@@ -72,29 +72,14 @@ if __name__ == '__main__':
         '--input_pairs', type=str, default='testdir/annos.txt',
         help='Path to the list of image pairs')
 
-    # parser.add_argument(
-    #     '--input_pairs', type=str, default='assets/scannet_sample_pairs_with_gt.txt',
-    #     help='Path to the list of image pairs')
-
     parser.add_argument(
         '--input_dir', type=str, default='testdir/imgs',
         help='Path to the directory that contains the images')
-    # parser.add_argument(
-    #     '--input_dir', type=str, default='assets/scannet_sample_images/',
-    #     help='Path to the directory that contains the images')
+    
     parser.add_argument(
         '--output_dir', type=str, default='testdir/imgs/',
         help='Path to the directory in which the .npz results and optionally,'
              'the visualization images are written')
-
-    # parser.add_argument(
-    #     '--max_length', type=int, default=-1,
-    #     help='Maximum number of pairs to evaluate')
-    # parser.add_argument(
-    #     '--resize', type=int, nargs='+', default=[480, 640],
-    #     help='Resize the input image before running inference. If two numbers, '
-    #          'resize to the exact dimensions, if one number, resize the max '
-    #          'dimension, if -1, do not resize')
 
     parser.add_argument(
         '--max_length', type=int, nargs='+', default=1280,
@@ -129,10 +114,6 @@ if __name__ == '__main__':
     parser.add_argument(
         '--viz', action='store_true',
         help='Visualize the matches and dump the plots')
-    # parser.add_argument(
-    #     '--eval', action='store_true',
-    #     help='Perform the evaluation'
-    #          ' (requires ground truth pose and intrinsics)')
     parser.add_argument(
         '--fast_viz', action='store_true',
         help='Use faster image visualization with OpenCV instead of Matplotlib')
@@ -167,17 +148,6 @@ if __name__ == '__main__':
     assert not (opt.fast_viz and not opt.viz), 'Must use --viz with --fast_viz'
     assert not (opt.fast_viz and opt.viz_extension == 'pdf'), 'Cannot use pdf extension with --fast_viz'
 
-    # if len(opt.resize) == 2 and opt.resize[1] == -1:
-    #     opt.resize = opt.resize[0:1]
-    # if len(opt.resize) == 2:
-    #     print('Will resize to {}x{} (WxH)'.format(
-    #         opt.resize[0], opt.resize[1]))
-    # elif len(opt.resize) == 1 and opt.resize[0] > 0:
-    #     print('Will resize max dimension to {}'.format(opt.resize[0]))
-    # elif len(opt.resize) == 1:
-    #     print('Will not resize images')
-    # else:
-    #     raise ValueError('Cannot specify more than two integers for --resize')
 
     with open(opt.input_pairs, 'r') as f:
         pairs = [l.split() for l in f.readlines()]
@@ -187,12 +157,6 @@ if __name__ == '__main__':
 
     if opt.shuffle:
         random.Random(0).shuffle(pairs)
-
-    # if opt.eval:
-    #     if not all([len(p) == 38 for p in pairs]):
-    #         raise ValueError(
-    #             'All pairs should have ground truth info for evaluation.'
-    #             'File \"{}\" needs 38 valid entries per row'.format(opt.input_pairs))
 
     # Load the SuperPoint and SuperGlue models.
     device = 'cuda' if torch.cuda.is_available() and not opt.force_cpu else 'cpu'
@@ -217,9 +181,7 @@ if __name__ == '__main__':
     output_dir = Path(opt.output_dir)
     output_dir.mkdir(exist_ok=True, parents=True)
     print('Will write matches to directory \"{}\"'.format(output_dir))
-    # if opt.eval:
-    #     print('Will write evaluation results',
-    #           'to directory \"{}\"'.format(output_dir))
+   
     if opt.viz:
         print('Will write visualization images to',
               'directory \"{}\"'.format(output_dir))
@@ -230,41 +192,13 @@ if __name__ == '__main__':
         name0, name1 = pair[:2]
         stem0, stem1 = Path(name0).stem, Path(name1).stem
         matches_path = output_dir / '{}_{}_match.jpg'.format(stem0, stem1)
-        # eval_path = output_dir / '{}_{}_evaluation.npz'.format(stem0, stem1)
         viz_path = output_dir / '{}_{}_matches.{}'.format(stem0, stem1, opt.viz_extension)
         viz_eval_path = output_dir / \
             '{}_{}_evaluation.{}'.format(stem0, stem1, opt.viz_extension)
 
         # Handle --cache logic.
         do_match = True
-        # do_eval = opt.eval
         do_viz = opt.viz
-        # do_viz_eval = opt.eval and opt.viz
-        # if opt.cache:
-        #     if matches_path.exists():
-        #         try:
-        #             results = np.load(matches_path)
-        #         except:
-        #             raise IOError('Cannot load matches .npz file: %s' %
-        #                           matches_path)
-
-        #         kpts0, kpts1 = results['keypoints0'], results['keypoints1']
-        #         matches, conf = results['matches'], results['match_confidence']
-        #         do_match = False
-        #     # if opt.eval and eval_path.exists():
-        #     #     try:
-        #     #         results = np.load(eval_path)
-        #     #     except:
-        #     #         raise IOError('Cannot load eval .npz file: %s' % eval_path)
-        #     #     err_R, err_t = results['error_R'], results['error_t']
-        #     #     precision = results['precision']
-        #     #     matching_score = results['matching_score']
-        #     #     num_correct = results['num_correct']
-        #     #     epi_errs = results['epipolar_errors']
-        #     #     do_eval = False
-        #     if opt.viz and viz_path.exists():
-        #         do_viz = False
-        #     timer.update('load_cache')
 
         if not (do_match or do_viz):
             timer.print('Finished pair {:5} of {:5}'.format(i, len(pairs)))
@@ -298,8 +232,6 @@ if __name__ == '__main__':
             # Write the matches to disk.
             out_matches = {'keypoints0': kpts0, 'keypoints1': kpts1,
                            'matches': matches, 'match_confidence': conf}
-            # pdb.set_trace()
-            # np.savez(str(matches_path), **out_matches)
 
             xy_F = kpts0
             xy_L = kpts1
@@ -321,21 +253,22 @@ if __name__ == '__main__':
             direct=warpImg.copy()
             direct[0:image0.shape[0], 0:image0.shape[1]] = image0
 
+            # 平滑处理
             rows,cols=image0.shape[:2]
             # drawMatches(image0, image1, PtsA, PtsB, Match, status)
             for row in range(0,rows):
-                if image0[row, :].any() and warpImg[row, :].any():#开始重叠的最左端
+                if image0[row, :].any() and warpImg[row, :].any():
                     top = row
                     break
             for row in range(rows-1, 0, -1):
-                if image0[row, :].any() and warpImg[row, :].any():#重叠的最右一列
+                if image0[row, :].any() and warpImg[row, :].any():
                     bot = row
                     break
 
             res = np.zeros([rows, cols], np.uint8)
             for col in range(0, cols):
                 for row in range(0, rows):
-                    if not image0[row, col].any():#如果没有原图，用旋转的填充
+                    if not image0[row, col].any():
                         res[row, col] = warpImg[row, col]
                     elif not warpImg[row, col].any():
                         res[row, col] = image0[row, col]
@@ -353,54 +286,6 @@ if __name__ == '__main__':
         mkpts0 = kpts0[valid]
         mkpts1 = kpts1[matches[valid]]
         mconf = conf[valid]
-
-        # if do_eval:
-        #     # Estimate the pose and compute the pose error.
-        #     assert len(pair) == 38, 'Pair does not have ground truth info'
-        #     K0 = np.array(pair[4:13]).astype(float).reshape(3, 3)
-        #     K1 = np.array(pair[13:22]).astype(float).reshape(3, 3)
-        #     T_0to1 = np.array(pair[22:]).astype(float).reshape(4, 4)
-
-        #     # Scale the intrinsics to resized image.
-        #     K0 = scale_intrinsics(K0, scales0)
-        #     K1 = scale_intrinsics(K1, scales1)
-
-        #     # Update the intrinsics + extrinsics if EXIF rotation was found.
-        #     if rot0 != 0 or rot1 != 0:
-        #         cam0_T_w = np.eye(4)
-        #         cam1_T_w = T_0to1
-        #         if rot0 != 0:
-        #             K0 = rotate_intrinsics(K0, image0.shape, rot0)
-        #             cam0_T_w = rotate_pose_inplane(cam0_T_w, rot0)
-        #         if rot1 != 0:
-        #             K1 = rotate_intrinsics(K1, image1.shape, rot1)
-        #             cam1_T_w = rotate_pose_inplane(cam1_T_w, rot1)
-        #         cam1_T_cam0 = cam1_T_w @ np.linalg.inv(cam0_T_w)
-        #         T_0to1 = cam1_T_cam0
-
-        #     epi_errs = compute_epipolar_error(mkpts0, mkpts1, T_0to1, K0, K1)
-        #     correct = epi_errs < 5e-4
-        #     num_correct = np.sum(correct)
-        #     precision = np.mean(correct) if len(correct) > 0 else 0
-        #     matching_score = num_correct / len(kpts0) if len(kpts0) > 0 else 0
-
-        #     thresh = 1.  # In pixels relative to resized image size.
-        #     ret = estimate_pose(mkpts0, mkpts1, K0, K1, thresh)
-        #     if ret is None:
-        #         err_t, err_R = np.inf, np.inf
-        #     else:
-        #         R, t, inliers = ret
-        #         err_t, err_R = compute_pose_error(T_0to1, R, t)
-
-        #     # Write the evaluation results to disk.
-        #     out_eval = {'error_t': err_t,
-        #                 'error_R': err_R,
-        #                 'precision': precision,
-        #                 'matching_score': matching_score,
-        #                 'num_correct': num_correct,
-        #                 'epipolar_errors': epi_errs}
-        #     np.savez(str(eval_path), **out_eval)
-        #     timer.update('eval')
 
         if do_viz:
             # Visualize the matches.
@@ -428,94 +313,3 @@ if __name__ == '__main__':
                 opt.fast_viz, opt.opencv_display, 'Matches', small_text)
 
             timer.update('viz_match')
-
-    #     if do_viz_eval:
-    #         # Visualize the evaluation results for the image pair.
-    #         color = np.clip((epi_errs - 0) / (1e-3 - 0), 0, 1)
-    #         color = error_colormap(1 - color)
-    #         deg, delta = ' deg', 'Delta '
-    #         if not opt.fast_viz:
-    #             deg, delta = '°', '$\\Delta$'
-    #         e_t = 'FAIL' if np.isinf(err_t) else '{:.1f}{}'.format(err_t, deg)
-    #         e_R = 'FAIL' if np.isinf(err_R) else '{:.1f}{}'.format(err_R, deg)
-    #         text = [
-    #             'SuperGlue',
-    #             '{}R: {}'.format(delta, e_R), '{}t: {}'.format(delta, e_t),
-    #             'inliers: {}/{}'.format(num_correct, (matches > -1).sum()),
-    #         ]
-    #         if rot0 != 0 or rot1 != 0:
-    #             text.append('Rotation: {}:{}'.format(rot0, rot1))
-
-    #         # Display extra parameter info (only works with --fast_viz).
-    #         k_thresh = matching.superpoint.config['keypoint_threshold']
-    #         m_thresh = matching.superglue.config['match_threshold']
-    #         small_text = [
-    #             'Keypoint Threshold: {:.4f}'.format(k_thresh),
-    #             'Match Threshold: {:.2f}'.format(m_thresh),
-    #             'Image Pair: {}:{}'.format(stem0, stem1),
-    #         ]
-
-    #         make_matching_plot(
-    #             image0, image1, kpts0, kpts1, mkpts0,
-    #             mkpts1, color, text, viz_eval_path,
-    #             opt.show_keypoints, opt.fast_viz,
-    #             opt.opencv_display, 'Relative Pose', small_text)
-
-    #         timer.update('viz_eval')
-
-    #     timer.print('Finished pair {:5} of {:5}'.format(i, len(pairs)))
-
-    # if opt.eval:
-    #     # Collate the results into a final table and print to terminal.
-    #     pose_errors = []
-    #     precisions = []
-    #     matching_scores = []
-    #     for pair in pairs:
-    #         name0, name1 = pair[:2]
-    #         stem0, stem1 = Path(name0).stem, Path(name1).stem
-    #         eval_path = output_dir / \
-    #             '{}_{}_evaluation.npz'.format(stem0, stem1)
-    #         results = np.load(eval_path)
-    #         pose_error = np.maximum(results['error_t'], results['error_R'])
-    #         pose_errors.append(pose_error)
-    #         precisions.append(results['precision'])
-    #         matching_scores.append(results['matching_score'])
-    #     thresholds = [5, 10, 20]
-    #     aucs = pose_auc(pose_errors, thresholds)
-    #     aucs = [100.*yy for yy in aucs]
-    #     prec = 100.*np.mean(precisions)
-    #     ms = 100.*np.mean(matching_scores)
-    #     print('Evaluation Results (mean over {} pairs):'.format(len(pairs)))
-    #     print('AUC@5\t AUC@10\t AUC@20\t Prec\t MScore\t')
-    #     print('{:.2f}\t {:.2f}\t {:.2f}\t {:.2f}\t {:.2f}\t'.format(
-    #         aucs[0], aucs[1], aucs[2], prec, ms))
-
-
-
-# npz_dir = r'testdir/imgs'
-# for i in os.listdir(npz_dir):
-#     if '.npz' in i:
-#         path = os.path.join(npz_dir,i)
-#         path = 'testdir/imgs/04010701_04010702_matches.npz'
-#         name = os.path.basename(os.path.splitext(path)[0])
-#         # fname,lname = name.split('_s')[0:2]
-#         # fname = os.path.join('assets\scannet_sample_images',fname+'.jpg')
-#         # lname = os.path.join('assets\scannet_sample_images','s'+lname.split('_matches')[0]+'.jpg')
-#         fname = os.path.join('testdir\imgs',name.split('_')[0]+'.jpg')
-#         lname = os.path.join('testdir\imgs',name.split('_')[1]+'.jpg')
-#         image0 = cv2.imread(fname)
-#         image1 = cv2.imread(lname)
-#         w = 480
-#         h = 640
-#         image0 = cv2.resize(image0,(w,h))
-#         image1 = cv2.resize(image1,(w,h))
-#         print(fname,lname)
-#         npz = np.load(path)
-#         # print(npz.files)
-#         # print(npz['keypoints0'][0])
-#         # # print(npz['matches']>-1)
-#         # print(npz['matches'].shape)
-#         # pdb.set_trace()
-        
-#         # show_Image_Cv(direct)
-#         # pdb.set_trace()
